964c562ddf0d6baeb35358404b3c93cd0f87ba44
Ahmed Yusef <ahmed.yusef.1993@gmail.com>
2021-09-08 04:13:49 +0300

    update data base

--- app/src/main/java/com/example/receiving_broadcasts/MainActivity.kt
+++ app/src/main/java/com/example/receiving_broadcasts/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.receiving_broadcasts
 
-import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.database.getStringOrNull
 import com.example.receiving_broadcasts.databinding.ActivityMainBinding
 import java.util.*
@@ -19,19 +18,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         dataBaseHelper = ArticleDbHelper(applicationContext)
-        addListeners()
+        val filter = IntentFilter()
+        filter.addAction(Intent.ACTION_SHUTDOWN)
+        registerReceiver(broadcastReceiver,filter)
         readData()
     }
 
-    private fun addListeners() {
-        val time = Calendar.getInstance().time
+    override fun onPause() {
+        super.onPause()
+        writeData()
+    }
+
+    private fun writeData(){
+        if (broadcastReceiver.isTrue()){
+            val time = Calendar.getInstance().time
         val newEntry = ContentValues().apply {
             put(Constant.TIME, time.toString())
         }
         dataBaseHelper.writableDatabase.insert(Constant.TABLE_NAME,null, newEntry)
-
+        }
     }
-    @SuppressLint("Recycle")
     private fun readData() {
         val cursor = dataBaseHelper.readableDatabase.rawQuery("SELECT * FROM ${Constant.TABLE_NAME}", arrayOf<String>())
         while (cursor.moveToNext()){
@@ -39,14 +45,5 @@ class MainActivity : AppCompatActivity() {
         binding.text.text = time
         }
     }
-    override fun onPostResume() {
-        super.onPostResume()
-        val filter = IntentFilter()
-        filter.addAction(Intent.ACTION_SHUTDOWN)
-        registerReceiver(broadcastReceiver,filter)
-    }
-    override fun onPause() {
-        super.onPause()
-        this.unregisterReceiver(broadcastReceiver)
-    }
+
 }
--- app/src/main/java/com/example/receiving_broadcasts/MyBroadcastReceiver.kt
+++ app/src/main/java/com/example/receiving_broadcasts/MyBroadcastReceiver.kt
@@ -1,42 +1,31 @@
 package com.example.receiving_broadcasts
 
 import android.content.BroadcastReceiver
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+import java.util.*
 
 
 class MyBroadcastReceiver : BroadcastReceiver() {
-    private var number = 0
-    private var count = 0
+    private var number: Int = 0
     override fun onReceive(p0: Context?, p1: Intent?) {
-        if (Intent.ACTION_REBOOT == p1?.action){
-            Toast.makeText(p0,"hello There",Toast.LENGTH_SHORT).show()
-                   number = 1
-                   count++
-        }else{
-            number = 2
+        when (p1?.action) {
+            Intent.ACTION_SHUTDOWN -> {
+                number = if (p1.extras?.getBoolean(Constant.STATE) == true) {
+                    Toast.makeText(p0, "hello There", Toast.LENGTH_SHORT).show()
+                    1
+                } else {
+                    Toast.makeText(p0, "No Action", Toast.LENGTH_SHORT).show()
+                    2
+                }
+            }
         }
-//        when(p1?.action){
-//            Intent.ACTION_SHUTDOWN -> {
-//               if (p1.extras?.getBoolean(Constant.STATE) == true){
-//                   Toast.makeText(p0,"hello There",Toast.LENGTH_SHORT).show()
-//                   number = 1
-//                   count++
-//               } else{
-//                   number = 2
-//               }
-//            }
-//        }
 
     }
-    fun textConvert (): String {
-        return if (number == 1){
-            "The devise Shut Down $count"
-        }else{
-            "The devise Not Shut Down"
-        }
+    fun isTrue(): Boolean {
+        return number == 2
     }
-
 
 }
